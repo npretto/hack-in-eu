7e9cd981ca1bc2b28c82c36f038e894181554b75
npretto <lordkryss@gmail.com>
2018-11-02 21:05:10 +0100

    added static .ics calendar

---
+++ src/scripts/build-calendar.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+import ical from "ical-generator";
+import { getHacks } from "../services/getHacks";
+
+const hacks = getHacks();
+
+const calendar = ical();
+
+calendar.events(
+  hacks.map(hack => ({
+    start: hack.startDate,
+    end: hack.endDate,
+    summary: hack.name,
+    description: hack.description,
+    location: `${hack.location.city}, ${hack.location.country}`,
+    url: hack.url,
+    allDay: true
+  }))
+);
+
+// console.log(calendar.toString());
+
+fs.writeFileSync("./public/hacks-in-eu.ics", calendar.toString());
